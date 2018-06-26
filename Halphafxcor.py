21558962a1ba34d1f8f94571705aa82ef4b213a2
Maddie Lucey <40609462+mlucey@users.noreply.github.com>
2018-06-26 13:48:38 -0500

    Update Halphafxcor.py

--- Halphafxcor.py
+++ Halphafxcor.py
@@ -24,17 +24,20 @@ inpath = '/Volumes/MADDIE/'+obsrun+'/images/'
 temp = '/Volumes/MADDIE/Forfxcor/1647952iplushalpha.fits'
 #where you want the fxcor outputs saved
 outpath = '/Volumes/MADDIE/'+obsrun+'/final_stuff/donefxcor/
-
+#where you want the list of halpha stars saved
+filepath = '/Volumes/MADDIE/'+obsrun+'/final_stuff/Halphalegend.txt'
+#where you have non-halpha rvcorrect.txt
+rvcorrect = '/Volumes/MADDIE/'+obsrun+'/final_stuff/rvcorrect.txt'
 
 halphafiles = []
 halphaaps = []
 halpharas = []
 
 
-hght = np.loadtxt('/Volumes/MADDIE/'+obsrun+'/final_stuff/rvcorrect.txt',usecols = (12,),dtype = str)
-files = np.loadtxt('/Volumes/MADDIE/'+obsrun+'/final_stuff/rvcorrect.txt',usecols = (13,),dtype = str)
-aps = np.loadtxt('/Volumes/MADDIE/'+obsrun+'/final_stuff/rvcorrect.txt',usecols = (10,))
-ra = np.loadtxt('/Volumes/MADDIE/'+obsrun+'/final_stuff/rvcorrect.txt',usecols = (9,),dtype = str)
+hght = np.loadtxt(rvcorrect,usecols = (12,),dtype = str)
+files = np.loadtxt(rvcorrect,usecols = (13,),dtype = str)
+aps = np.loadtxt(rvcorrect,usecols = (10,))
+ra = np.loadtxt(rvcorrect,usecols = (9,),dtype = str)
 
 #grabs spectra that failed to get an rv when matched with a template without halpha emission
 for i in range(len(hght)):
@@ -45,7 +48,7 @@ for i in range(len(hght)):
         
         
 #runs fxcor and writes a file with the ras of each star used 
-file = open('/VOlumes/MADDIE/'+obsrun+'/final_stuff/Halphalegend.txt','w')
+file = open(filepath,'w')
 for k in range(len(halphafiles)):
     iraf.rv.fxcor(inpath+halphafiles[k]+'_nosky.fits',temp,apertures = str(int(halphaaps[k])),output = outpath+"halpha_"+halphafiles[k]+"_"+str(halpharas[k]),verbose='txtonly')
     file.write("%8s %8s %8.3f \n" % (halpharas[k],'1647952i',-71.084))
