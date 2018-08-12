f01f133794ce82db41535d1a64aaff41de4ef7a8
Maddie Lucey <40609462+mlucey@users.noreply.github.com>
2018-08-12 17:16:31 -0500

    Update sun_rvs.py

--- sun_rvs.py
+++ sun_rvs.py
@@ -9,22 +9,26 @@ iraf.rv()
 
 #get skyflat rvs
 
-obsrun = 'Mar2018'
-outpath = '/Users/Natalie/mann/fxcor_rv/'+obsrun+'/results/'
-#where you have the skyflat image
-imagepath = '/Users/Natalie/mann/fxcor_rv/'+obsrun+'/'
-#template your using
-temp = '/Users/Natalie/mann/templates/new_sunspec.fits'
+#**need to edit obsrun and the paths**
 
-fits = fits.open(imagepath+'RED.ms.fits')
-head = fits[0].header
-numstars = head['NAXIS2']
+obsrun = ['Apr2018','Mar2018','Dec2016','Feb2017','Feb2016','Mar2016']
 
-aps = []
-for i in range(numstars):
-    splits = head['APNUM'+str(i+1)].split(' ')
-    ap = splits[0]
-    aps.append(ap)
-for j in range(len(aps)):
-    iraf.rv.fxcor(imagepath+'RED.ms.fits' ,temp, apertures=aps[j] ,output= outpath+'Reds'+str(aps[j]) , function='gaussian', background='INDEF', verbose = 'txtonly', interactive = 'no')
+for t in obsrun:
+    outpath = '/Volumes/MADDIE/'+t+'/results/donefxcor/'
+    #where you have the skyflat image
+    imagepath = '/Volumes/MADDIE/'+t+'/images/'
+    #template your using
+    temp = '/Volumes/MADDIE/newtemplates/new_sunspec.fits'
+
+    fitsfile = fits.open(imagepath+'RED.ms.fits')
+    head = fitsfile[0].header
+    numstars = head['NAXIS2']
+
+    aps = []
+    for i in range(numstars):
+        splits = head['APNUM'+str(i+1)].split(' ')
+        ap = splits[0]
+        aps.append(ap)
+    for j in range(len(aps)):
+        iraf.rv.fxcor(imagepath+'RED.ms.fits' ,temp, apertures=aps[j] ,output= outpath+'Reds'+str(aps[j]) , function='gaussian', background='INDEF', verbose = 'txtonly', interactive = 'no')
 
